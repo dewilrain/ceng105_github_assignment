06c50324066f723ab1d162a1294964ed1fa133f3
dewilrain <dewilrain1@gmail.com>
2022-11-21 22:28:13 +0300

    implemented sum operation

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	printf("hello world\n")
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	printf("hello world\n");
+	printf("enter a and b: ");
+	scanf("%d %d", &a, &b);
+	printf("sum of a and b: %d\n", a+b);
 	return 0;
 }
